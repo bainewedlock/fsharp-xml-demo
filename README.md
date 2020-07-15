09ac0b0054189ae18e167f052ed211867a17cfa7
boeckwi <w.boeck@walz.de>
2020-07-15 08:16:19 +0200

    Add project files.

---
+++ README.md
@@ -0,0 +1,44 @@
+### naiveApproach
+time for 1000 loops: 00:00:00.1894996
+<?xml version="1.0" encoding="utf-16"?>
+<ArrayOfInt xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+  <int>1</int>
+  <int>2</int>
+  <int>3</int>
+  <int>4</int>
+  <int>5</int>
+</ArrayOfInt>
+
+### naiveApproachWithCustomRootElement
+time for 1000 loops: 00:00:15.0232130
+<?xml version="1.0" encoding="utf-16"?>
+<MyData xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+  <int>1</int>
+  <int>2</int>
+  <int>3</int>
+  <int>4</int>
+  <int>5</int>
+</MyData>
+
+### datacontractApproach
+time for 1000 loops: 00:00:00.1206794
+<ArrayOfint xmlns:i="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://schemas.microsoft.com/2003/10/Serialization/Arrays">
+  <int>1</int>
+  <int>2</int>
+  <int>3</int>
+  <int>4</int>
+  <int>5</int>
+</ArrayOfint>
+
+### withPicklerLibrary
+time for 1000 loops: 00:00:00.4434285
+<?xml version="1.0" encoding="utf-16"?>
+<FsPickler version="4.0.0.0" type="System.Int32[]">
+  <value flags="sequence">
+    <elem>1</elem>
+    <elem>2</elem>
+    <elem>3</elem>
+    <elem>4</elem>
+    <elem>5</elem>
+  </value>
+</FsPickler>
---
+++ XmlDemo/Program.fs
@@ -0,0 +1,69 @@
+﻿open System
+open System.IO
+open System.Xml
+open System.Xml.Serialization
+open System.Runtime.Serialization
+open System.Diagnostics
+
+// requires: reference to System.Xml.Serialization
+let naiveApproach (ob:'a) =
+    let s = new System.Xml.Serialization.XmlSerializer(typeof<'a>)
+    use writer = new StringWriter()
+    s.Serialize(writer, ob)
+    writer.ToString()
+
+// requires: reference to System.Xml.Serialization
+let naiveApproachWithCustomRootElement (ob:'a) =
+    let root = XmlRootAttribute()
+    root.ElementName <- "MyData"
+    let s = new System.Xml.Serialization.XmlSerializer(typeof<'a>,root)
+    use writer = new StringWriter()
+    s.Serialize(writer, ob)
+    writer.ToString()
+
+// requires: reference to System.Runtime.Serialization
+let datacontractApproach (ob:'a) =
+    let ser = new DataContractSerializer(typeof<'a>)
+    use sw = new StringWriter()
+    use xw = new XmlTextWriter(sw)
+    xw.Formatting <- Formatting.Indented
+    ser.WriteObject(xw, ob)
+    sw.ToString()
+
+// requires: install-package FsPickler
+let withPicklerLibrary ob = 
+    let serializer = MBrace.FsPickler.FsPickler.CreateXmlSerializer(indent = true)
+    use writer = new StringWriter()
+    serializer.Serialize(writer, ob)
+    writer.ToString()
+
+let testObject = [| 1; 2; 3; 4; 5|]
+
+let currentlyUsedRam () =
+    use proc = Process.GetCurrentProcess()
+    use PC = new PerformanceCounter()
+    PC.CategoryName <- "Process"
+    PC.CounterName <- "Working Set - Private"
+    PC.InstanceName <- proc.ProcessName
+    PC.NextValue() |> int
+
+let benchmark title callback =
+    let started = DateTimeOffset.Now
+    let n = 1000
+    for _ in [1..n] do callback testObject |> ignore
+    let finished = DateTimeOffset.Now
+    [
+        sprintf "### %s" title
+        sprintf "time for %d loops: %A" n (finished - started)
+        callback testObject
+        ""
+    ]
+    |> List.iter (printfn "%s")
+
+[<EntryPoint>]
+let main argv =
+    benchmark "naiveApproach" naiveApproach
+    benchmark "naiveApproachWithCustomRootElement" naiveApproachWithCustomRootElement
+    benchmark "datacontractApproach" datacontractApproach
+    benchmark "withPicklerLibrary" withPicklerLibrary
+    0
